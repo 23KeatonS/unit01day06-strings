e9025a94dc3274e485ccc67cc09b531d1345a583
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-23 11:43:07 -0600

    finished NegatePhrase

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,5 +1,11 @@
 public class NegatePhrase {
     public static void main(String[] args) {
-        
+        String str = args[0];
+        String strBeg = str.substring(0,3);
+        if (strBeg.equals("not")){
+            System.out.println(str);
+        }else{
+            System.out.println("not "+str);
+        }
     }
 }
