0542fd82b1f9f943fcdaf05b5a33f8382b2f5d05
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-23 11:47:01 -0600

    finished abbreviate

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,12 @@
 public class Abbreviate {
     public static void main(String[] args) {
-        
+        String str = args[0];
+        if (str.length()<8){
+            System.out.println(str);
+        }else{
+            String strBeg = str.substring(0,2);
+            String strEnd = str.substring(str.length()-2,str.length());
+            System.out.println(strBeg+"..."+strEnd);
+        }
     }
 }
