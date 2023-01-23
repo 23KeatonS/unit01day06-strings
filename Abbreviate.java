b9e6dad5bbcba7559a0862e53721680d04e781df
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-23 12:11:40 -0600

    made new string

--- Abbreviate.java
+++ Abbreviate.java
@@ -2,11 +2,13 @@ public class Abbreviate {
     public static void main(String[] args) {
         String str = args[0];
         if (str.length()<8){
-            System.out.println(str);
+            String finalstr = str;
+            System.out.println(finalstr);
         }else{
             String strBeg = str.substring(0,2);
             String strEnd = str.substring(str.length()-2,str.length());
-            System.out.println(strBeg+"..."+strEnd);
+            String finalstr = strBeg+"..."+strEnd;
+            System.out.println(finalstr);
         }
     }
 }
