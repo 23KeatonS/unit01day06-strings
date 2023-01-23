cd28463932f07c240983afc9fea263f7a60573d3
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-23 11:39:23 -0600

    finished shortlongshort

--- ShortLongShort.java
+++ ShortLongShort.java
@@ -1,5 +1,11 @@
 public class ShortLongShort {
     public static void main(String[] args) {
-        
+     String str1 = args[0];
+     String str2 = args[1];
+     if (str1.length()>str2.length()){
+        System.out.println(str2 +str1+str2);
+     }else{
+        System.out.println(str1+str2+str1);
+     }   
     }
 }
